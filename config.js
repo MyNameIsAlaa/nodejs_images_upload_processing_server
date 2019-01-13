0133d5758019c243dfa8fbb48d912e2727c9e4f4
alaa <alaa0selim@gmail.com>
2019-01-13 12:39:51 +0200

    fix upload url

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config = {
         URL: process.env.mongoURL || "mongodb://alaa:alaa123@cluster0-shard-00-00-yunyh.mongodb.net:27017,cluster0-shard-00-01-yunyh.mongodb.net:27017,cluster0-shard-00-02-yunyh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
     },
     app:{
-        upload_url: process.env.upload_url || "https://image-processing-server.herokuapp.com/upload/"
+        upload_url: process.env.upload_url || "https://image-processing-server.herokuapp.com/api/files/"
     }
 }
 

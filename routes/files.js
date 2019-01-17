e112d5e22fc2a39b9082d9c11325e95431db2c99
alaa <alaa0selim@gmail.com>
2019-01-17 12:41:11 +0200

    add scale endpoint

--- routes/files.js
+++ routes/files.js
@@ -14,8 +14,6 @@ Router.get('/:file', (req,res)=>{
 });
 
 
-
-
 Router.post('/upload', (req,res)=>{
     
   files = [],
@@ -147,9 +145,52 @@ Router.post('/crop', [
         Jimp.read(path.join(UploadDir + '/' + file)).then((image)=>{
             image.crop( x, y, width, height)
             .write(path.join(UploadDir +  '/' + perfix + file));
-           return resolve(perfix + file);
+            return resolve(perfix + file);
          }).catch((error)=>{
-           return reject(error);
+            return reject(error);
+        })
+      })
+   });
+   
+   Promise.all(promises).then((results)=>{
+    res.status(200).json(results);
+   }).catch((error)=>{
+    res.status(422).json(error);
+   });
+
+});
+
+
+
+
+Router.post('/scale', [
+  check('size').isNumeric(),
+  check('files').not().isEmpty(),
+], (req, res)=>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  let files = JSON.parse(req.body['files']);
+  let size = parseFloat(req.body['size']);
+  let UploadDir = path.join( __dirname + '/../uploads');
+  let perfix =  'scale_' + String(size).replace('.', '_') + '_';
+
+
+  var promises = files.map(file => {
+      return new Promise((resolve, reject)=>{
+
+        if(fs.existsSync(path.join(UploadDir + '/' + perfix + file))){
+         return resolve(perfix + file); // file already croped before so no need to continue
+        }
+
+        Jimp.read(path.join(UploadDir + '/' + file)).then((image)=>{
+            image.scale(size)
+            .write(path.join(UploadDir +  '/' + perfix + file));
+            return resolve(perfix + file);
+         }).catch((error)=>{
+            return reject(error);
         })
       })
    });

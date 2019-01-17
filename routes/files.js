b5da1a62d5c164d2a463987d9fa2942447e2ffd2
alaa <alaa0selim@gmail.com>
2019-01-17 12:11:35 +0200

    crop and resize endpoints

--- routes/files.js
+++ routes/files.js
@@ -1,9 +1,10 @@
-var Router = require("express").Router();
-var path = require('path');
-var fs = require('fs');
-var formidable = require('formidable');
-var FileDB = require("../db/files");
-
+const Router = require("express").Router();
+const path = require('path');
+const fs = require('fs');
+const formidable = require('formidable');
+const FileDB = require("../db/files");
+const { check, validationResult } = require('express-validator/check');
+const Jimp = require('jimp');
 
 Router.get('/:file', (req,res)=>{
     fs.exists(__dirname + '/../uploads/'  + req.params.file, (exists)=>{
@@ -69,5 +70,96 @@ Router.post('/upload', (req,res)=>{
 });
  
 
+
+Router.post('/resize', [
+  check('width').isNumeric(),
+  check('height').isNumeric(),
+  check('files').not().isEmpty(),
+], (req, res)=>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  let files = JSON.parse(req.body['files']);
+  let width = parseInt(req.body['width']);
+  let height = parseInt(req.body['height']);
+  let UploadDir = path.join( __dirname + '/../uploads');
+  let perfix = width + '_' + height + '_';
+
+  var promises = files.map(file => {
+      return new Promise((resolve, reject)=>{
+
+        if(fs.existsSync(path.join(UploadDir + '/' + perfix + file))){
+          return resolve(perfix + file); // file already resized before so no need to continue
+        }
+
+        Jimp.read(path.join(UploadDir + '/' + file)).then((image)=>{
+            image.resize( width, height )
+            .write(path.join(UploadDir + '/' + perfix + file));
+          return resolve(perfix + file);
+         }).catch((error)=>{
+         return reject(error);
+        })
+      })
+   });
+   
+   Promise.all(promises).then((results)=>{
+    res.status(200).json(results);
+   }).catch((error)=>{
+    res.status(422).json(error);
+   });
+
+});
+
+
+
+
+
+Router.post('/crop', [
+  check('width').isNumeric(),
+  check('height').isNumeric(),
+  check('x').isNumeric(),
+  check('y').isNumeric(),
+  check('files').not().isEmpty(),
+], (req, res)=>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  let files = JSON.parse(req.body['files']);
+  let width = parseInt(req.body['width']);
+  let height = parseInt(req.body['height']);
+  let x = parseInt(req.body['x']);
+  let y = parseInt(req.body['y']);
+  let UploadDir = path.join( __dirname + '/../uploads');
+  let perfix =  'corp_' + x + '_' + y + '_' + width + '_' + height + '_';
+
+
+  var promises = files.map(file => {
+      return new Promise((resolve, reject)=>{
+
+        if(fs.existsSync(path.join(UploadDir + '/' + perfix + file))){
+         return resolve(perfix + file); // file already croped before so no need to continue
+        }
+
+        Jimp.read(path.join(UploadDir + '/' + file)).then((image)=>{
+            image.crop( x, y, width, height)
+            .write(path.join(UploadDir +  '/' + perfix + file));
+           return resolve(perfix + file);
+         }).catch((error)=>{
+           return reject(error);
+        })
+      })
+   });
+   
+   Promise.all(promises).then((results)=>{
+    res.status(200).json(results);
+   }).catch((error)=>{
+    res.status(422).json(error);
+   });
+
+});
 
 module.exports = Router;

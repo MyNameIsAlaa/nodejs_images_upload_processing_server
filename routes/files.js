08e349a8635218a7876666e8acf020c02584da29
alaa <alaa0selim@gmail.com>
2019-01-18 17:52:29 +0200

    add response to blur endpoint

--- routes/files.js
+++ routes/files.js
@@ -251,6 +251,12 @@ Router.post('/blur', [
   });
 
 
+  Promise.all(promises).then((results) => {
+    res.status(200).json(results);
+  }).catch((error) => {
+    res.status(422).json(error);
+  });
+
 })
 
 module.exports = Router;
